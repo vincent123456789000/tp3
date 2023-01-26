51697f36448e419813bf4f33fbcfb32f5415ccd6
vincent123456789000 <113608065+vincent123456789000@users.noreply.github.com>
2023-01-26 17:44:22 -0500

    Update tp3.py

--- tp3.py
+++ tp3.py
@@ -1,27 +1,27 @@
 import random
 game_loop = True
-lvl_life = 300
 combat_nbr = 20
-victory_nbr = 30
 defeat_nbr = 24
-nombre_victoires_consecutives = 0
-afficher_regle = False
-force_adversaire = 0
-monstre_fort = False
-victoire_monstre_fort = 0
+number_of_consecutive_victories = 0
+victory_nbr = 30
+lvl_life = 300
+rule_show = False
+victoire_strength_monster = 0
+strength_monster = False
+opponent_strength = 0
 
 while game_loop:
-   if not afficher_regle:
-       if not monstre_fort:
-           force_adversaire = random.randint(1, 5) + random.randint(1, 5)
-           print(f"Vous tombez face à face avec un adversaire de difficulté : {force_adversaire}")
+   if not rule_show:
+       if not strength_monster:
+           opponent_strength = random.randint(1, 5) + random.randint(1, 5)
+           print(f"Vous tombez face à face avec un adversaire de difficulté : {opponent_strength}")
        else:
-           force_adversaire = random.randint(4, 5) + random.randint(4, 5)
-           print(f"Vous tombez face à face avec un adversaire de haut niveau : {force_adversaire}")
-           monstre_fort = False
+           opponent_strength = random.randint(4, 5) + random.randint(4, 5)
+           print(f"Vous tombez face à face avec un adversaire de haut niveau : {opponent_strength}")
+           strength_monster = False
    else:
-       afficher_regle = False
-       print(f"Vous tombez face à face avec un adversaire de difficulté : {force_adversaire}")
+       rule_show = False
+       print(f"Vous tombez face à face avec un adversaire de difficulté : {opponent_strength}")
 
    option_menu = int(input("""Que voulez-vous faire ?
 1- Combattre cet adversaire
@@ -32,25 +32,25 @@ while game_loop:
 
    if option_menu == 1:
        combat_nbr += 1
-       print(f"Force de l'adversaire : {force_adversaire} \n"
+       print(f"Force de l'adversaire : {opponent_strength} \n"
              f"Niveau de vie : {lvl_life} \n"
              f"Combat {combat_nbr} : {victory_nbr} vs {defeat_nbr}")
        dice_combat = random.randint(1, 6) + random.randint(1, 6)
        print(f"Lancer du dé : {dice_combat} ")
 
-       if dice_combat > force_adversaire:
+       if dice_combat > opponent_strength:
            print("Dernier combat = Victoire")
-           lvl_life += force_adversaire
-           nombre_victoires_consecutives += 1
-           victoire_monstre_fort += 1
+           lvl_life += opponent_strength
+           number_of_consecutive_victories += 1
+           victoire_strength_monster += 1
            victory_nbr += 1
            print(f"Niveau de vie : {lvl_life}\n"
-                 f"Nombre de victoires consécutives : {nombre_victoires_consecutives}")
+                 f"Nombre de victoires consécutives : {number_of_consecutive_victories}")
 
        else:
            print("Dernier combat = Défaite")
-           nombre_victoires_consecutives = 0
-           lvl_life -= force_adversaire
+           number_of_consecutive_victories = 0
+           lvl_life -= opponent_strength
            defeat_nbr += 1
            print(f"Niveau de vie : {lvl_life}")
 
@@ -69,13 +69,13 @@ while game_loop:
            "La partie se termine lorsque les points de vie de l’usager tombent sous 0.\n"
            "L’usager peut combattre ou éviter chaque adversaire, dans le cas de l’évitement, "
            "il y a une pénalité de 1 point de vie.\n")
-       afficher_regle = True
+       rule_show = True
    else:
        game_loop = False
 
-   if victoire_monstre_fort == 3:
-       monstre_fort = True
-       victoire_monstre_fort = 0
+   if victoire_strength_monster == 3:
+       strength_monster = True
+       victoire_strength_monster = 0
 
    if lvl_life <= 0:
        print(f"La partie est terminée, vous avez vaincu {victory_nbr} monstres")
